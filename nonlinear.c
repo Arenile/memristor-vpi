03cb24a18398f88d7426d64486cf123cf84b1bba
Ivris Raymond <theivris@gmail.com>
2023-04-11 19:50:32 -0500

    Made some code prettier

--- nonlinear.c
+++ nonlinear.c
@@ -24,35 +24,16 @@
 double nonlinear(double vDec, double vInc, double dt, double prev_state) {
 
     double vIn = vInc - vDec;
-
     double M = ((R_OFF * prev_state) + (R_ON * (1 - prev_state)));
-
     double i = vIn / M;
-
-    // Window function should go here but instead...
-
-    // if i < * THRESHOLD *
-    // then not hight enough current to change M
-    // else
-    // M should change based on how high i is and
-    // some other constants 
-
     double new_state = prev_state;
 
-    // Need window function for nonlinear behavior
-
+    // Need window functin for nonlinear behavior
     double window = (1 - pow((2*(prev_state+0.00001) - 1), (2*P)));
-
     double change = K*(i*dt)*window;
 
-    // printf("change = %f\n", change);
-    // printf("i = %f\n", i);
-    // printf("M = %f\n", M);
-    // printf("window = %f\n", window);
-
     if (fabs(i) < THRESHOLD) {
-        printf("NOT CHANGED\n");
-        return new_state;
+        printf("NO CHANGE\n");
     }
     else if (i < 0) {
         M = M - change;
@@ -65,7 +46,6 @@ double nonlinear(double vDec, double vInc, double dt, double prev_state) {
         }
 
         new_state = (M - R_ON) / (R_OFF - R_ON);
-        return new_state;
     }
     else {
         M = M + change;
@@ -78,8 +58,9 @@ double nonlinear(double vDec, double vInc, double dt, double prev_state) {
         }
 
         new_state = (M - R_ON) / (R_OFF - R_ON);
-        return new_state;
     }
+
+    return new_state;
 
 }
 
